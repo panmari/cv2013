cd5d0fe45dc33fcffac1dc0ffb09f49e31501ee3
panmari <smmuzi@gmail.com>
2013-10-17 19:22:26 +0200

    implemented computation of centroid
    For finding bright spots on sphere and radius and center of sphere etc.

--- assignement_1/A1_Code/fitChromeSphere.m
+++ assignement_1/A1_Code/fitChromeSphere.m
@@ -21,12 +21,63 @@ function [L] = fitChromeSphere(chromeDir, nDir, chatty)
 
   for n=1:nDir
     fname = [chromeDir,'chrome.',num2str(n-1),'.png'];
-    im = imread(fname);
+    im = imread(fname, 'png');
     imData(:,:,n) = im(:,:,1);           % red channel
   end
-
-  size(imData)
+  [xmax, ymax, nr_images] = size(imData);
+  
+  %% computes centroid of bright spot
+  for img_nr=1:nr_images
+    bright_spots(:, :, img_nr) = getCentroid(imData(:,:,img_nr), mask); % fix inversion on y axis
+  end
+  %% compute center and radius of sphere
+  sphere_center = getCentroid(mask, ones(xmax, ymax), 0);
+  sphere_min = [10000, 10000];
+  sphere_max = [0, 0];
+  for x=1:xmax
+    for y=1:ymax
+        if mask(x,y) == 1
+            if  x < sphere_min(1)
+                sphere_min(1) = x;
+            end
+            if  y < sphere_min(2)
+                sphere_min(2) = y;
+            end
+            if  x > sphere_max(1)
+                sphere_min(1) = x;
+            end
+            if  y > sphere_max(2)
+                sphere_min(2) = y;
+            end
+        end
+    end
+  end
+  diameter_xy = sphere_min - sphere_max;
+  radius_xy = diameter_xy/2;
+  
   % YOU NEED TO COMPLETE THIS FUNCTION
+  L = bright_spots; 
+end
 
-  return;
+%% Returns the centroid of the bright blob inside the image
+function centroid = getCentroid(img, mask, threshold)
+  if nargin < 3
+    threshold = 240;  
+  end
+  [xmax, ymax] = size(img);
+  weights_sum = 0;
+  bright_spot = [0.0, 0.0];
+  for x=1:xmax
+      for y=1:ymax
+          weight = double(img(x,y));
+          % ignore blue highlight
+          if weight < threshold || mask(x,y) == 0
+              weight = 0;
+          end
+         bright_spot = bright_spot + [x,y]*weight;
+         weights_sum = weights_sum + weight;
+      end
+  end
+  centroid = bright_spot/weights_sum;
+end
 
