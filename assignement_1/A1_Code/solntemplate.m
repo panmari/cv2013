462fcd267c2e846177b10e2c046b2840ec68a5b1
panmari <smmuzi@gmail.com>
2013-10-19 23:06:53 +0200

    some skeleton code for next step

--- assignement_1/A1_Code/solntemplate.m
+++ assignement_1/A1_Code/solntemplate.m
@@ -3,7 +3,21 @@
 clear all;
 close all;
 
-
+nDir = 12;
 
 % Put your code here
-fitChromeSphere('../Images/chrome/', 12, true)
+%method = 0; % default light directions
+method = 1; % computed light directions from chrome sphere
+L = getLightDir(method,'../Images/chrome/', nDir, true);
+
+img_name = 'buddha'
+img_folder = ['../Images/', img_name, '/']
+mask = imread([img_folder, img_name, '.mask.png']);
+mask = mask(:,:,1) / 255.0;
+
+for n=1:nDir
+    fname = [img_folder, img_name, '.',num2str(n-1),'.png'];
+    im = imread(fname, 'png');
+    imData(:,:,n) = im(:,:,1);           % red channel
+end
+
