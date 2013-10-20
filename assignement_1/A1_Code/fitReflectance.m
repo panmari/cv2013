4713210fd8a50f16cf1bb4fc1d59214c40fc9785
panmari <smmuzi@gmail.com>
2013-10-20 12:18:47 +0200

    computing now grayscale normals and grayscale albedo

--- assignement_1/A1_Code/fitReflectance.m
+++ assignement_1/A1_Code/fitReflectance.m
@@ -11,7 +11,19 @@ function [n, albedo] = fitReflectance(im, L)
     
 
   % YOU NEED TO COMPLETE THIS
-
-  return;
-
+  
+  % We know im(x) = albedo(x)(normal(x)*L), for every pixel x
+  [x_max, y_max, ndir] = size(im);
+  albedo = zeros([x_max,y_max, 1]);
+  n = zeros([x_max,y_max, 3]);
+  %im1D = reshape(im, [], ndir);
+  inv_lights = pinv(L)';
+  parfor x=1:x_max
+      for y=1:y_max
+          n_tilde = inv_lights*reshape(im(x,y,:), [], 1);
+          albedo(x,y,:) = norm(n_tilde, 2);
+          n(x,y,:) = n_tilde/albedo(x,y,:);
+      end
+  end
+end
 
--- assignement_1/A1_Code/solntemplate.m
+++ assignement_1/A1_Code/solntemplate.m
@@ -10,14 +10,26 @@ nDir = 12;
 method = 1; % computed light directions from chrome sphere
 L = getLightDir(method,'../Images/chrome/', nDir, true);
 
-img_name = 'buddha'
-img_folder = ['../Images/', img_name, '/']
+img_name = 'buddha';
+img_folder = ['../Images/', img_name, '/'];
 mask = imread([img_folder, img_name, '.mask.png']);
 mask = mask(:,:,1) / 255.0;
 
-for n=1:nDir
+parfor n=1:nDir
     fname = [img_folder, img_name, '.',num2str(n-1),'.png'];
-    im = imread(fname, 'png');
-    imData(:,:,n) = im(:,:,1);           % red channel
+    im = double(imread(fname, 'png'));
+    imLuminances(:,:,n) = im(:,:,1)*0.299+im(:,:,2)*0.587+im(:,:,3)*0.114;  % computes luminance of each pixel
 end
+
+%% compute normals and albedo in grayscale
+% todo: use masks?
+[n, albedo] = fitReflectance(imLuminances, L);
+% and show them
+imshow((n+1)/2);
+imshow(albedo/max(max(albedo)));
+
+%% TODO: compute albedo of all color channels by using normal
+
+
+
 
