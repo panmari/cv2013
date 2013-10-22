ff125102cca7c341dada991a7c24beebc739a688
panmari <smmuzi@gmail.com>
2013-10-22 17:20:42 +0200

    refactored loops to matlab style matrix functions

--- assignement_1/A1_Code/fitReflectance.m
+++ assignement_1/A1_Code/fitReflectance.m
@@ -8,22 +8,15 @@ function [n, albedo] = fitReflectance(im, L)
   %   n - nPix x 3 array of surface normals, with n(k,1:3) = (nx, ny, nz)
   %       at the k-th pixel.
   %   albedo - nPix x 1 array of estimated albdedos
-    
-
-  % YOU NEED TO COMPLETE THIS
-  
+      
   % We know im(x) = albedo(x)(normal(x)*L), for every pixel x
   [x_max, y_max, ndir] = size(im);
-  albedo = zeros([x_max,y_max, 1]);
-  n = zeros([x_max,y_max, 3]);
-  %im1D = reshape(im, [], ndir);
-  inv_lights = pinv(L)';
-  parfor x=1:x_max
-      for y=1:y_max
-          n_tilde = inv_lights*reshape(im(x,y,:), [], 1);
-          albedo(x,y,:) = norm(n_tilde, 2);
-          n(x,y,:) = n_tilde/albedo(x,y,:);
-      end
-  end
+  im1D = reshape(im, [], ndir);
+
+  n_tilde1D = L'\im1D';
+  albedo1D = sqrt(sum(n_tilde1D.^2,1)); 
+  n1D = bsxfun(@rdivide, n_tilde1D, albedo1D);
+  albedo = reshape(albedo1D, [x_max, y_max, 1]);
+  n = reshape(n1D', [x_max, y_max, 3]);
 end
 
