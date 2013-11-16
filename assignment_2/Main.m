6ac3d5aed99cb4a5be799ff396ae41a938e5c9d4
panmari <smmuzi@gmail.com>
2013-11-16 14:14:40 +0100

    changed way of computing epipolar line tiny bit

--- assignment_2/Main.m
+++ assignment_2/Main.m
@@ -1,5 +1,5 @@
 
-scene = 'tsukuba'; % either tsukuba or rock;
+scene = 'corridor'; % either tsukuba or rock;
 img_left = imread(['Stereo Pairs/' scene '-l.tiff']);
 img_right = imread(['Stereo Pairs/' scene '-r.tiff']);
  %% let user specify corresponding points   
@@ -8,15 +8,17 @@ img_right = imread(['Stereo Pairs/' scene '-r.tiff']);
 %% Left is base image
 F = getFundamentalMatrix(left, right);
 %% compute epipolar lines
-point = [308, 40];
+point = [108, 150]; %on left img (base)
 imshow(img_left)
 hold on
 plot(point(1), point(2), 'or')
-l = F*[point ,1]';
+l = F*[point ,1]'; 
 x1 = -l(3)/l(1);
-x2 = (-288*l(2) - l(3))/l(1);
+y2 = -l(3)/l(2);
 figure, imshow(img_right)
 hold on
-plot([x1,x2], [0,288], 'r')
+[width, height] = size(img_right);
+plot([x1, 0], [0, y2], 'r')
 %% compute epipoles
-epipoles = null(F); 
+epipole_l = null(F); 
+epipole_r = null(F'); 
