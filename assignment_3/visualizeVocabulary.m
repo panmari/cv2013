7155a17294ba27b15b5c427ffde2e6dfe5834af6
panmari <smmuzi@gmail.com>
2013-11-25 15:19:41 +0100

    fixed for more than 25 entries, k easier choosable

--- assignment_3/visualizeVocabulary.m
+++ assignment_3/visualizeVocabulary.m
@@ -3,16 +3,19 @@ load('sift/twoFrameData.mat')
 run('vlfeat-0.9.17/toolbox/vl_setup')
 im = single(rgb2gray(im1));
 %% cluster!
-[centers, assignments] = vl_kmeans(descriptors1', 1500/20);
+k = 1500/20; % number of clusters
+[centers, assignments] = vl_kmeans(descriptors1', k);
 %% show words
-for i=1:2
+for i=randsample(1:k, 2)
     idxs = find(assignments == i);
     figure
     subplotpos = 1;
     for j=idxs
         d = getPatchFromSIFTParameters(positions1(j,:), scales1(j,:), orients1(j,:), im);
-        subplot(5,5,subplotpos);
-        imshow(d/255);
+        if subplotpos <= 25
+            subplot(5,5,subplotpos);
+            imshow(d/255);
+        end
         subplotpos = subplotpos + 1;
     end
     figure
