14692bce700c3bae516550238ed7c8ad71e92bea
panmari <smmuzi@gmail.com>
2013-11-26 13:23:01 +0100

    removed threshold, showing now always 10 best matches
    also printing mean square error of 10 best matches

--- assignment_3/rawDescriptorMatches.m
+++ assignment_3/rawDescriptorMatches.m
@@ -2,17 +2,16 @@
 load('sift/twoFrameData.mat')
 selected = selectRegion(im1, positions1);
 
-%% Match descriptors on other image and display
+%% Match descriptors on other image
 % TODO: possibly remove matched descs from descriptors2
 idxs = [];
 for desc=descriptors1(selected,:)'
     sum_diff2 = dist2(desc', descriptors2);
     [c, i] = min(sum_diff2);
-    % if below some threshold, add match
-    if (c < 0.2)
-        idxs = [idxs, i];
-    end
+    idxs = [idxs; c, i];
 end
-sprintf('Found %d matches', length(idxs))
-%idxs = sum(dist) < 240;
+%% Display 10 best matches
+idxs = sortrows(idxs, 1);
+fprintf('Mean error for 10 best matches: %f \n', mean(idxs(1:10, 1)));
+idxs = idxs(1:10, 2);
 displaySIFTPatches(positions2(idxs,:),  scales2(idxs,:), orients2(idxs,:), im2);
