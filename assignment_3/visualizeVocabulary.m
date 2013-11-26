010019d6ae30c575a5d22f5d5d87ca7a164df3c4
panmari <smmuzi@gmail.com>
2013-11-26 13:40:08 +0100

    visualizing error for different k sized bags

--- assignment_3/visualizeVocabulary.m
+++ assignment_3/visualizeVocabulary.m
@@ -2,9 +2,17 @@
 load('sift/twoFrameData.mat')
 run('vlfeat-0.9.17/toolbox/vl_setup')
 im = single(rgb2gray(im1));
-%% cluster!
-k = 1500/20; % number of clusters
-[centers, assignments] = vl_kmeans(descriptors1', k);
+%% Show error for different cluster sizes
+es = [];
+for k=1:100
+    [~, ~, e] = vl_kmeans(descriptors1', k);
+    es = [es, e];
+end
+plot(es);
+%% Cluster with k=25 for showing bags
+k = 25;
+[centers, assignments, e] = vl_kmeans(descriptors1', k);
+fprintf('Made %d words, error: %f \n', k, e);
 %% show words
 for i=randsample(1:k, 2)
     idxs = find(assignments == i);
