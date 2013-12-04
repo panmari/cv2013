0ae22c984ad6d43fb549d3ae5a4e0a16e00d256d
panmari <smmuzi@gmail.com>
2013-12-04 16:11:33 +0100

    rewrote algorithm to do clusterin over whole database of descriptors
    ...among multiple images
    Also using integer version of clustering algorithm (faster?)

--- assignment_3/visualizeVocabulary.m
+++ assignment_3/visualizeVocabulary.m
@@ -1,31 +1,46 @@
 %% Initialize workspace and vl
-load('sift/twoFrameData.mat')
 run('vlfeat-0.9.17/toolbox/vl_setup')
-im = single(rgb2gray(im1));
-%% Show error for different cluster sizes
-es = [];
-for k=1:100
-    [~, ~, e] = vl_kmeans(descriptors1', k);
-    es = [es, e];
+%% Concatenate descriptors of all images
+framesdir = 'images/';
+siftdir = 'sift/';
+% Get a list of all the .mat files in that directory.
+% There is one .mat file per image.
+fnames = dir([siftdir '/*.mat']);
+
+fprintf('reading %d total files...\n', length(fnames));
+all_descriptors = [];
+all_positions = [];
+all_scales = [];
+all_orients = [];
+img_idxs = [];
+for i = 1:length(fnames)
+    load([siftdir fnames(i).name], 'imname', 'descriptors', 'positions', 'scales', 'orients','numfeats');
+    all_descriptors = [all_descriptors, descriptors];
+    all_positions = [all_positions, positions];
+    all_scales = [all_scales, scales];
+    all_orients = [all_orients, orients];
+    imgs(:,:,i) = single(rgb2gray(imread(imname)));
+    img_idxs = [img_idxs, repmat(i, [1 length(descriptors)])];
 end
-plot(es);
-%% Cluster with k=25 for showing bags
-k = 25;
-[centers, assignments, e] = vl_kmeans(descriptors1', k);
-fprintf('Made %d words, error: %f \n', k, e);
+%% Clustering with k clusters for showing bags
+k = 1500;
+fprintf('Clustering %d words with %d descriptors, this might take some time...', k, length(all_descriptors));
+[centers, assignments] = vl_ikmeans(all_descriptors, k);
+fprintf('Done!');
 %% show words
 for i=randsample(1:k, 2)
     idxs = find(assignments == i);
     figure
     subplotpos = 1;
     for j=idxs
-        d = getPatchFromSIFTParameters(positions1(j,:), scales1(j,:), orients1(j,:), im);
+        % find image belonging to idx j
+        img_idx = img_idxs(j);
+        d = getPatchFromSIFTParameters(all_positions(:,j), all_scales(:,j), ...
+            all_orients(:,j), imgs(:,:,img_idx));
         if subplotpos <= 25
             subplot(5,5,subplotpos);
-            imshow(d/255);
+            imshow(d);
         end
         subplotpos = subplotpos + 1;
     end
-    figure
-    displaySIFTPatches(positions1(idxs,:),  scales1(idxs,:), orients1(idxs,:), im1);
 end
