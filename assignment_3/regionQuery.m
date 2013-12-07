6c05e459c9a4b9b97601d809bb950cc55d9de2fc
panmari <smmuzi@gmail.com>
2013-12-07 16:12:24 +0100

    making subplot tighter
    and some smaller fixes

--- assignment_3/regionQuery.m
+++ assignment_3/regionQuery.m
@@ -1,14 +1,14 @@
 %% Make bag of words
 %run('bagOfWords')
 %% Assemble histograms (same as in FullFrameQueries)
+nrImages = size(imgs, 3);
 histograms = assembleHistograms(assignments, centers, img_idxs, nrImages);
 
 %% select region, compute bag of words of region
-nrImages = size(imgs, 3);
 queryImg = 15;
 positions = all_positions(:, img_idxs == queryImg);
 selected = selectRegion(imgs(:,:,queryImg)/255, positions');
-fprintf('Number of descriptors in region: %d', sum(selected));
+fprintf('Number of descriptors in region: %d \n', length(selected));
 assignments_img = assignments(:, img_idxs == queryImg);
 assignments_region = assignments_img(:, selected);
 hist = histc(assignments_region, 1:length(centers));
@@ -22,13 +22,13 @@ weighted_hist = hist/sum(hist).*log(sum(hist_all)./hist_all);
 similarities = computeSimilarities(histograms, weighted_hist', nrImages);
 
 %% Show 3 images with closest histograms
+subplot = @(m,n,p) subtightplot (m, n, p, [0.01 0.05], [0.1 0.01], [0.1 0.01]);
 figure
-iptsetpref('ImshowBorder','tight');
 subplot(2,3, 2);
-imshow(imgs(:,:,queryImg), [0 255]);
+imshow(imgs(:,:,queryImg), [0 255], 'border', 'tight');
 hold on;
 scatter(positions(1,selected), positions(2,selected), 'r.');
 for similarImg=1:3
     subplot(2, 3, 3 + similarImg);
-    imshow(imgs(:,:,similarities(similarImg, 2)), [0 255]);
+    imshow(imgs(:,:,similarities(similarImg, 2)), [0 255], 'border', 'tight');
 end
