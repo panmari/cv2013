763b6eeba5f99c39eca9e0cda857e2e2f346b0b5
panmari <smmuzi@gmail.com>
2013-12-07 11:03:40 +0100

    fixed bug with region selection of region query
    also computing histogram slightly different (only change in scale)

--- assignment_3/regionQuery.m
+++ assignment_3/regionQuery.m
@@ -4,15 +4,15 @@
 %% select region, compute bag of words of region
 nrImages = size(imgs, 3);
 queryImg = 30;
-positions = all_positions(:, img_idxs == img);
+positions = all_positions(:, img_idxs == queryImg);
 selected = selectRegion(imgs(:,:,queryImg)/255, positions');
-assignments_img = assignments(:, img_idxs == img);
+assignments_img = assignments(:, img_idxs == queryImg);
 assignments_region = assignments_img(:, selected);
 hist = histc(assignments_region, 1:length(centers));
 
 %% term frequency - inverse document frequency
 hist_all = histc(assignments, 1:length(centers));
-weighted_hist = -hist.*log(hist_all/sum(hist_all));
+weighted_hist = hist/sum(hist).*log(sum(hist_all)/hist_all);
 
 %% find images that match weighted hist closest
 similarities = computeSimilarities(histograms, weighted_hist', nrImages);
