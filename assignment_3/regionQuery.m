7b2dfcd21f866cc73b5f65df3b39d1c760ca6d42
panmari <smmuzi@gmail.com>
2013-12-10 14:09:37 +0100

    using tf-idf weighted histograms again
    and setting some titles for figures

--- assignment_3/regionQuery.m
+++ assignment_3/regionQuery.m
@@ -1,5 +1,5 @@
 %% Choose query img
-queryImg = 25;
+queryImg = 15;
 %% Make bag of words
 %run('bagOfWords')
 %% Assemble histograms (same as in FullFrameQueries)
@@ -8,16 +8,17 @@ histograms = assembleHistograms(assignments, centers, img_idxs, nrImages);
 
 %% select region, compute bag of words of region
 positions = all_positions(:, img_idxs == queryImg);
+figure('name', 'Select an interesting region!');
 selected = selectRegion(imgs(:,:,queryImg)/255, positions');
-fprintf('Number of descriptors in region: %d \n', length(selected));
+%fprintf('Number of descriptors in region: %d \n', length(selected));
 assignments_img = assignments(:, img_idxs == queryImg);
 assignments_region = assignments_img(:, selected);
 hist = histc(assignments_region, 1:length(centers));
 
 %% term frequency - inverse document frequency
 hist_all = histc(assignments, 1:length(centers));
-%weighted_hist = hist.*log(sum(hist_all)./hist_all); %/sum(hist) % would normalize
-weighted_hist = hist;
+weighted_hist = hist.*log(sum(hist_all)./hist_all); %/sum(hist); % would normalize
+%weighted_hist = hist;
 %% show histograms, mainly for debugging
 % top: unweighted region
 % middle: of all images
