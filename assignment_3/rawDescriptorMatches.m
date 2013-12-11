4634804a887a877389ce77b7ba4fa1983867de50
panmari <smmuzi@gmail.com>
2013-12-11 17:35:48 +0100

    extended visualization of rawDescriptorMatches for doing report

--- assignment_3/rawDescriptorMatches.m
+++ assignment_3/rawDescriptorMatches.m
@@ -1,5 +1,6 @@
 %% Initialize workspace and select region
 load('twoFrameData.mat')
+figure
 selected = selectRegion(im1, positions1);
 
 %% Match descriptors on other image
@@ -12,6 +13,9 @@ for desc=descriptors1(selected,:)'
 end
 %% Display 10 best matches
 idxs = sortrows(idxs, 1);
-fprintf('Mean error for 10 best matches: %f \n', mean(idxs(1:10, 1)));
-idxs = idxs(1:10, 2);
-displaySIFTPatches(positions2(idxs,:),  scales2(idxs,:), orients2(idxs,:), im2);
+fprintf('Mean error for all matches: %f \n', mean(idxs(:, 1)));
+idxs_show = idxs %idxs(1:10, :); % top ten
+fprintf('Mean error for 10 best matches: %f \n', mean(idxs_show(:, 1)));
+figure
+displaySIFTPatches(positions2(idxs_show(:,2),:), ...
+    scales2(idxs_show(:,2),:), orients2(idxs_show(:,2),:), im2);
